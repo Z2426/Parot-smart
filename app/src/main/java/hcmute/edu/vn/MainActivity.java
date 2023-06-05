a364e3af4731f2e5ca5beda306e746413cb0eec5
Z2426 <nguyenlehuy812@gmail.com>
2023-06-05 22:51:50 +0700

    realtime

--- app/src/main/java/hcmute/edu/vn/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         iv_mic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                speak();
+                speak(source);
             }
         });
 
@@ -229,10 +229,10 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
-    private void speak() {
+    private void speak(String source) {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, source);
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please Speaking");
 
         try {
